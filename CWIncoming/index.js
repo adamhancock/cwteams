c306fd672714c6961f71fe6a1f87d1999cd2b45b
Adam Hancock <adam@ingeniotech.co.uk>
2019-04-03 17:42:58 +0100

    logging

--- CWIncoming/index.js
+++ CWIncoming/index.js
@@ -1,18 +1,17 @@
 var axios = require('axios');
 
-module.exports = function (context, req) {
+module.exports = function(context, req) {
+	var td = JSON.parse(req.body.Entity);
+	context.log(req.body);
 
-    var td = JSON.parse(req.body.Entity);
+	var teamsdata = {
+		title: `Ticket #${td.Id} from ${td.ContactName} @ ${td.CompanyName}`,
+		text: `Summary: ${td.Summary} // BoardName: ${td.BoardName} // Status: ${td.StatusName}`
+	};
 
-    var teamsdata = {
-        "title": `Ticket #${td.Id} from ${td.ContactName} @ ${td.CompanyName}`,
-        "text": `Summary: ${td.Summary} // BoardName: ${td.BoardName} // Status: ${td.StatusName}`
-    }
+	if (td.BoardName) {
+		axios.post(process.env[td.BoardName], teamsdata);
+	}
 
-    if (td.BoardName) {
-        axios.post(process.env[td.BoardName], teamsdata)
-    }
-
-
-    context.done();
+	context.done();
 };

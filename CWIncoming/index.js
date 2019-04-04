cb5b19c2484496a7aa5417663c8aa075d94b5deb
Adam Hancock <adam@ingeniotech.co.uk>
2019-04-04 21:02:59 +0100

    added open ticket button

--- CWIncoming/index.js
+++ CWIncoming/index.js
@@ -3,11 +3,25 @@ const duplicateCheck = require('../lib/duplicateCheck');
 
 module.exports = async function(context, req) {
 	var td = JSON.parse(req.body.Entity);
-	context.log(req.body);
-
+	const body = req.body;
+	context.log(
+		`https://${body.FromUrl}/v4_6_release/services/system_io/Service/fv_sr100_request.rails?service_recid=${body.ID}&companyName=${body.CompanyId}`
+	);
 	var teamsdata = {
 		title: `Ticket #${td.Id} from ${td.ContactName} @ ${td.CompanyName}`,
-		text: `Summary: ${td.Summary} // BoardName: ${td.BoardName} // Status: ${td.StatusName}`
+		text: `Summary: ${td.Summary} // BoardName: ${td.BoardName} // Status: ${td.StatusName}`,
+		potentialAction: [
+			{
+				'@type': 'OpenUri',
+				name: 'Open Ticket',
+				targets: [
+					{
+						os: 'default',
+						uri: `https://${body.FromUrl}/v4_6_release/services/system_io/Service/fv_sr100_request.rails?service_recid=${body.ID}&companyName=${body.CompanyId}`
+					}
+				]
+			}
+		]
 	};
 
 	if (await duplicateCheck(context, td.Id)) {
@@ -19,6 +33,7 @@ module.exports = async function(context, req) {
 		context.done();
 	} else {
 		if (td.BoardName) {
+			//context.log(process.env);
 			axios.post(process.env[td.BoardName], teamsdata);
 		}
 		context.res = {
